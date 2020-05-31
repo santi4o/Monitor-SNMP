eb5ced6f3c17239216086580c0608be94fc6875a
Aldo <aldo11santiago@gmail.com>
2020-05-30 19:26:54 -0500

    snmp python script working for Cisco Routers 15.6(2)T

---
+++ snmp/ciscooids.py
@@ -0,0 +1,5 @@
+ObjectType(ObjectIdentity('.1.3.6.1.4.1.9.9.109.1.1.1.1.8.1')), #El porcentaje de ocupado de la CPU total en el periodo de cinco minutos
+ObjectType(ObjectIdentity('.1.3.6.1.4.1.9.9.48.1.1.1.5.1')),#memoria usada cpu (cisco)
+ObjectType(ObjectIdentity('.1.3.6.1.4.1.9.9.48.1.1.1.5.2')),#memoria usada i/o (cisco)
+ObjectType(ObjectIdentity('.1.3.6.1.4.1.9.9.48.1.1.1.6.1')),#memoria libre cpu (cisco)
+ObjectType(ObjectIdentity('.1.3.6.1.4.1.9.9.48.1.1.1.6.2')))#memoria libre i/o (cisco)
